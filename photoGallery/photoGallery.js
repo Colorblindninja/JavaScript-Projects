e5ddc0b91334500543a8e28c0271990cd7a24227
Zach Lamoreaux <Zach Lamoreaux>
2016-02-29 10:29:10 -0700

    started work, set up the jquery to start to make the page along with adding a very simple css and html page

--- photoGallery/photoGallery.js
+++ photoGallery/photoGallery.js
@@ -1,10 +1,47 @@
 "use strict";
-for (var cat_index in piclist) {
-    var category = piclist[cat_index];
-    for (var image_index in category) {
-        var imagepair = category[image_index];
-        var fullsize = baseurl + imagepair[0];
-        var thumbnail = baseurl + imagepair[1];
-        document.writeln('<img src="' + thumbnail + '">');
+jQuery(document).ready(function($){
+  // for (var cat_index in piclist) {
+  //   var category = piclist[cat_index];
+  //   for (var image_index in category) {
+  //     var imagepair = category[image_index];
+  //     var fullsize = baseurl + imagepair[0];
+  //     var thumbnail = baseurl + imagepair[1];
+  //     document.writeln('<img src="' + thumbnail + '">');
+  //   }
+  // }
+
+
+  var setUp = function() {
+    $(".category").css("height", (100/piclist.length) + "%");
+    var categoryContainer = $(".categoryContainer");
+    var categories = [];
+    var thumbnailContainers = [];
+    for (var cat_index in piclist) {
+      //make a new category under the categoryContainer and give it the image of the first thing in the respective list in piclist
+      categories.push($('<li class="category"> Category '+(cat_index+1)+'</li>'));
+      $(".category:nth-of-type("+(cat_index+1)+")").css("background","url("+baseurl + piclist[cat_index][1][0]+")").css("background-size","cover");
+      categoryContainer.append(categories[cat_index]);
+
+      //make the thumbnail container for the thumbnails that will appear if the use clicks on the category
+      thumbnailContainers.push($('<ul class+"thumbnailContainer">'));
+      for (var image_index in piclist[cat_index]) {
+        //might need to fix to get the right thumbnails because of scoping, add in which category from the html
+        var thumbnail = $('<li class="thumbnail"> Thumbnail '+(image_index+1)+'</li>');
+        $(".thumbnail:nth-of-type("+(image_index+1)+")").css("background","url("+baseurl + piclist[cat_index][image_index][1]+")").css("background-size","cover");
+        //$(".thumbnail:nth-of-type("+(image_index+1)+")").css("background","url("baseurl + piclist[cat_index][image_index][1]")").css("background-size","cover");
+        thumbnailContainers[cat_index].append(thumbnail);
+      }
+
+      //add the click handler for each category to make the respective set of thumbnails appear
+      categories[cat_index].on("click", function() {
+        this.slideUp();
+        thumbnailContainers[cat_index].slideDown();
+      });
     }
-}
+  }
+
+  setUp();
+
+
+  return false;
+});
