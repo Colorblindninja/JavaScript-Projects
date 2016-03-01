7644773017a2ee96de9d2747c3dd90fc439a6f60
Zachary Lamoreaux <zlamoreaux@niobium.cs.dixie.edu>
2016-02-29 22:00:48 -0700

    made a lot of stuff

--- photoGallery/photoGallery.js
+++ photoGallery/photoGallery.js
@@ -12,34 +12,69 @@ jQuery(document).ready(function($){
 
 
   var setUp = function() {
-    $(".category").css("height", (100/piclist.length) + "%");
+    //$(".category").css("height", (100/piclist.length) + "%");
     var categoryContainer = $(".categoryContainer");
     var categories = [];
     var thumbnailContainers = [];
-    for (var cat_index in piclist) {
-      //make a new category under the categoryContainer and give it the image of the first thing in the respective list in piclist
-      categories.push($('<li class="category"> Category '+(cat_index+1)+'</li>'));
-      $(".category:nth-of-type("+(cat_index+1)+")").css("background","url("+baseurl + piclist[cat_index][1][0]+")").css("background-size","cover");
-      categories[cat_index].appendTo("ul.categoryContainer");
+    $(".thumbnailContainer").each(function (index) {
+        $(this).hide(0).attr({
+            id: "Thumbnail Container "+index
+        })
+    });
 
-      //make the thumbnail container for the thumbnails that will appear if the use clicks on the category
-      thumbnailContainers.push($('<ul class="thumbnailContainer">'));
-      thumbnailContainers[cat_index].appendTo("li.category");
-      for (var image_index in piclist[cat_index]) {
-        //might need to fix to get the right thumbnails because of scoping, add in which category from the html
-        var thumbnail = $('<li class="thumbnail"> Thumbnail '+(image_index+1)+'</li>');
-        $(".thumbnail:nth-of-type("+(image_index+1)+")").css("background","url("+baseurl + piclist[cat_index][image_index][1]+")").css("background-size","cover");
-        //$(".thumbnail:nth-of-type("+(image_index+1)+")").css("background","url("baseurl + piclist[cat_index][image_index][1]")").css("background-size","cover");
-        //thumbnailContainers[cat_index].append(thumbnail);
-        thumbnail.appendTo("ul.thumbnailContainer")
-      }
-
-      //add the click handler for each category to make the respective set of thumbnails appear
-      categories[cat_index].on("click", function() {
-        this.slideUp();
-        thumbnailContainers[cat_index].slideDown();
-      });
-    }
+    // $('.category').each(function (index){
+    //     $(this).attr({
+    //           id: 'Category '+index
+    //       })
+    //       .css("background", "transparent url("+baseurl + piclist[index][1][0]+")")
+    //       .click(function () {
+    //           $(this).slideUp('fast', function() {
+    //               $(this).slideDown('fast');
+    //             })
+    //             .css('background', '')
+    //             .addClass('.thumbnailContainer')
+    //             .removeClass('.category');
+    //         });
+    //     });
+                //   }).slideDown('fast', function() {
+                //       for (var image_index in piclist[index]) {
+                //           var imagepair = piclist[index][image_index];
+                //           var thumbnail = baseurl + imagepair[1];
+                //           var thumb = $('<img src="' + thumbnail + '" class="thumbnail">');
+                //           thumb.click(function() {
+                //             //itll do something
+                //           });
+                //           $(this).append(thumb);
+                //       }
+                //   });
+    $('.category').each(function (index){
+        $(this).attr({
+              id: 'Category '+index
+          })
+          .css("background", "transparent url("+baseurl + piclist[index][1][0]+")")
+          .click(function () {
+              $(this).slideUp('fast', function() {
+                  $(document.getElementById("Thumbnail Container "+index)).click(function() {
+                      $(this).slideUp('fast', function() {
+                          $(document.getElementById('Category '+index)).slideDown('fast');
+                          $(this).children().remove();
+                      });
+                  })
+                  .slideDown('fast', function() {
+                      for (var image_index in piclist[index]) {
+                          var imagepair = piclist[index][image_index];
+                          var thumbnail = baseurl + imagepair[1];
+                          var thumb = $('<img src="' + thumbnail + '" class="thumbnail">');
+                          thumb.click(function(index, image_index) {
+                              var img = $('<img src="'+baseurl+piclist[index][image_index][0]+'" class="viewer">');
+                              $('body').append(img);
+                          });
+                          $(this).append(thumb);
+                      }
+                  });
+              });
+          });
+    });
   }
 
   setUp();
