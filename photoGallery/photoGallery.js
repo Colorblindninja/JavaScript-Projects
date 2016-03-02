42343a6fd8a4bbd04800a7a48a2ff37985ddb076
Zachary Lamoreaux <zlamoreaux@niobium.cs.dixie.edu>
2016-03-01 22:37:40 -0700

    almost finished basic functionality

--- photoGallery/photoGallery.js
+++ photoGallery/photoGallery.js
@@ -9,8 +9,6 @@ jQuery(document).ready(function($){
   //     document.writeln('<img src="' + thumbnail + '">');
   //   }
   // }
-
-
   var setUp = function() {
     //$(".category").css("height", (100/piclist.length) + "%");
     var categoryContainer = $(".categoryContainer");
@@ -21,37 +19,28 @@ jQuery(document).ready(function($){
             id: "Thumbnail Container "+index
         })
     });
+    $(".viewerContainer").hide(0);
+    $(".viewer").hide(0);
+    $(".exit").hide(0);
+    $(".slideshow").hide(0);
+    $('.slideshow').click(function(){
+        if (startSlideshow){
+          window.clearInterval(startSlideshow);
+      } else {
+          var startSlideshow = setInterval(function(){
+                $(".viewer").trigger("click");
+          }, 1000);
+      }
+    });
 
-    // $('.category').each(function (index){
-    //     $(this).attr({
-    //           id: 'Category '+index
-    //       })
-    //       .css("background", "transparent url("+baseurl + piclist[index][1][0]+")")
-    //       .click(function () {
-    //           $(this).slideUp('fast', function() {
-    //               $(this).slideDown('fast');
-    //             })
-    //             .css('background', '')
-    //             .addClass('.thumbnailContainer')
-    //             .removeClass('.category');
-    //         });
-    //     });
-                //   }).slideDown('fast', function() {
-                //       for (var image_index in piclist[index]) {
-                //           var imagepair = piclist[index][image_index];
-                //           var thumbnail = baseurl + imagepair[1];
-                //           var thumb = $('<img src="' + thumbnail + '" class="thumbnail">');
-                //           thumb.click(function() {
-                //             //itll do something
-                //           });
-                //           $(this).append(thumb);
-                //       }
-                //   });
+
     $('.category').each(function (index){
         $(this).attr({
               id: 'Category '+index
           })
-          .css("background", "transparent url("+baseurl + piclist[index][1][0]+")")
+          .css("background", "url("+baseurl + piclist[index][1][0]+")")
+          .css("background-repeat", "no-repeat")
+          .css("background-size", "cover")
           .click(function () {
               $(this).slideUp('fast', function() {
                   $(document.getElementById("Thumbnail Container "+index)).click(function() {
@@ -65,17 +54,44 @@ jQuery(document).ready(function($){
                           var imagepair = piclist[index][image_index];
                           var thumbnail = baseurl + imagepair[1];
                           var thumb = $('<img src="' + thumbnail + '" class="thumbnail">');
-                          thumb.click(function(index, image_index) {
-                              var img = $('<img src="'+baseurl+piclist[index][image_index][0]+'" class="viewer">');
-                              $('body').append(img);
-                          });
                           $(this).append(thumb);
                       }
+                      $(this).children(".thumbnail").each(function(image_index){
+                          $(this).click(function() {
+                              var img = $('<img src="'+baseurl+piclist[index][image_index][0]+'" class="viewer">');
+                              var counter = 0;
+                              var outerCounter = 0;
+                              $('body').append(img);
+                              $(img).click(function(){
+                                  counter++;
+                                  $(img).fadeOut('fast', function(){
+                                      if (((counter+image_index)%piclist[index+outerCounter].length)==0) {
+                                          outerCounter++;
+                                          outerCounter = outerCounter %piclist.length;
+                                          counter = 0;
+                                          image_index = 0;
+                                      }
+                                      $(this).attr("src",baseurl+piclist[(index+outerCounter)][(image_index+counter)%piclist[(index+outerCounter)].length][0]);
+                                      $(img).fadeIn('fast');
+                                  });
+                              });
+                              $('.exit').show(0).click(function(){
+                                //   $(this).hide(0);
+                                //   $('.slideshow').hide(0);
+                                //   $('.viewer').hide(0);
+                                //   window.clearInterval(startSlideshow);
+                                reload();
+                              });
+                              $('.slideshow').show(0);
+                              console.log(index +" "+ image_index);
+                              return false;
+                          });
+                      });
                   });
               });
           });
-    });
-  }
+      });
+    }
 
   setUp();
 
