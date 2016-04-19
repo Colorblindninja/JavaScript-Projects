48cf25ed030d9387399ea4aa9f0143210342e6de
Zach Lamoreaux <zachlamoreaux@hotmail.com>
2016-04-19 17:52:48 -0600

    started the final project

--- finalProject/final.js
+++ finalProject/final.js
@@ -1,15 +1,29 @@
 "use strict";
 
-var setUp = function() {
+var fourWayStop = function() {
 
-return;
+    var horizontalRoad = $('<div class="horizontalRoad"/>')
+        .css('top', '400px')
+        .css('left', '200px')
+        .css('width', '500px');
+
+    var verticalRoad = $('<div class="verticalRoad"/>')
+        .css('top', '100px')
+        .css('left', '400px')
+        .css('height', '500px');
+
+    $('.sim').append(horizontalRoad).append(verticalRoad);
+    return;
 
 }
+
+
 
 $(document).ready(function() {
 
     $('.quizContainer').hide();
 
     $('.sim').append($('<div class = "mainCar"/>'));
+    fourWayStop();
 
 });
