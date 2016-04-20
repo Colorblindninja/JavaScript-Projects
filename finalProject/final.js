5e25252ba9bcc70acc3c31bda5f359cac28c00be
Zach Lamoreaux <zachlamoreaux@hotmail.com>
2016-04-19 22:01:04 -0600

    animated the cars, made a 4way stop function?

--- finalProject/final.js
+++ finalProject/final.js
@@ -8,12 +8,41 @@ var fourWayStop = function() {
         .css('width', '500px');
 
     var verticalRoad = $('<div class="verticalRoad"/>')
-        .css('top', '100px')
+        .css('top', '200px')
         .css('left', '400px')
         .css('height', '500px');
 
-    $('.sim').append(horizontalRoad).append(verticalRoad);
-    return;
+    var mainCar = $('<div class="verticalMainCar"/>')
+        .css('top', '650px')
+        .css('left', '462px');
+
+
+    var topCar = $('<div class="verticalCar"/>')
+        .css('top', '200px')
+        .css('left', '412px');
+
+    var leftCar = $('<div class="horizontalCar"/>')
+        .css('top', '462px')
+        .css('left', '200px');
+
+    var rightCar = $('<div class="horizontalCar"/>')
+        .css('top', '412px')
+        .css('left', '650px');
+
+    $('.sim').append(horizontalRoad).append(verticalRoad).append(mainCar).append(topCar).append(leftCar).append(rightCar);
+
+    mainCar.animate({
+        top: "-=150px",
+    }, 1000 );
+    topCar.animate({
+        top: "+=150px",
+    }, 1000 );
+    leftCar.animate({
+        left: "+=150px",
+    }, 1000 );
+    rightCar.animate({
+        left: "-=150px",
+    }, 1000 );
 
 }
 
@@ -24,6 +53,8 @@ $(document).ready(function() {
     $('.quizContainer').hide();
 
     $('.sim').append($('<div class = "mainCar"/>'));
-    fourWayStop();
+
+    
+    fourWayStop()
 
 });
